64b655ba9b0b176d644ff5079042a27ee9bad282
sappir <sappirbohbot@gmail.com>
2021-11-12 22:01:10 +0200

    added repr2

---
+++ Call.py
@@ -0,0 +1,11 @@
+
+
+class Call:
+    def __init__(self, call = "", time= 0.0 , src = 0, dest = 0, elevator = 0,F=-1 ):
+        self.call =call
+        self.time = time
+        self.src = src
+        self.dest = dest
+        self.elevator = elevator
+        self.F = F
+
