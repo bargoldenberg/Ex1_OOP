b0487f993e720629056361ae02bf5d2cf2ca3f47
sappir <sappirbohbot@gmail.com>
2021-11-12 18:49:48 +0200

    added repr

--- Building.py
+++ Building.py
@@ -7,7 +7,8 @@ class Building:
         self._maxFloor = 0
         self._elevators = [] # matching type as the Json
 
-
+    def __repr__(self):
+        return f"minFloor:{self._minFloor} maxFloor:{self._maxFloor} elevators: {self._elevators}"
     def load_json(self,file_name):
         try:
             with open(file_name , "r+") as f:
@@ -16,13 +17,11 @@ class Building:
                 self._minFloor = my_d["_minFloor"]
                 self._maxFloor = my_d["_maxFloor"]
                 ele_l = my_d["_elevators"]
-                #for elem in ele_l:
                 for v in ele_l:
-                   # print(ele_l)
-                    #print(v["_id"])
-                    e=Elevator(id=v["_id"], speed=v["_speed"], minFloor=v["_minFloor"], maxFloor=v["_maxFloor"],
-                               closeTime=v["_closeTime"], openTime=v["_openTime"], startTime=v["_startTime"], stopTime=v["_stopTime"])
-                   # e = Elevator(**v)
+
+                    #e=Elevator(id=v["_id"], speed=v["_speed"], minFloor=v["_minFloor"], maxFloor=v["_maxFloor"],
+                     #          closeTime=v["_closeTime"], openTime=v["_openTime"], startTime=v["_startTime"], stopTime=v["_stopTime"])
+                    e = Elevator(**v)
                     my_e.append(e)
             self._elevators = my_e
         except IOError as e:
@@ -34,4 +33,4 @@ self,id: int=0,speed: float=0,minFloor: int =0,maxFloor: int =0,closeTime: float
 
 b=Building()
 b.load_json("B1.json")
-print(b._elevators.pop())
+print(b)
--- Elevator.py
+++ Elevator.py
@@ -1,27 +1,28 @@
 import json
 
 class Elevator:
-    def __init__(self,id: int=0,speed: float=0,minFloor: int =0,maxFloor: int =0,closeTime: float=0,openTime: float=0, startTime: float=0,stopTime: float=0) -> None:
-        self._id=id
-        self._speed=speed
-        self._minFloor=minFloor
-        self._maxFloor=maxFloor
-        self._closeTime=closeTime
-        self._openTime=openTime
-        self._startTime=startTime
-        self._stopTime=stopTime
-
+    def __init__(self,_id: int=0,_speed: float=0,_minFloor: int =0,_maxFloor: int =0,_closeTime: float=0,_openTime: float=0, _startTime: float=0,_stopTime: float=0) -> None:
+        self.id=_id
+        self.speed=_speed
+        self.minFloor=_minFloor
+        self.maxFloor=_maxFloor
+        self.closeTime=_closeTime
+        self.openTime=_openTime
+        self.startTime=_startTime
+        self.stopTime=_stopTime
+    def __repr__(self):
+        return f"id:{self.id} speed:{self.speed} minFloor:{self.minFloor} maxFloor:{self.maxFloor} closeTime:{self.closeTime} openTime:{self.openTime} startTime:{self.startTime} stopTime:{self.stopTime} "
     def from_json(self, file_name):
         with open(file_name, "r") as fp:
             di = json.load(fp)
-            self._id = di["_id"]
-            self._speed = di["_speed"]
-            self._minFloor = di["_minFloor"]
-            self._maxFloor = di["_maxFloor"]
-            self._closeTime = di["_closeTime"]
-            self._openTime = di["_openTime"]
-            self._startTime = di["_startTime"]
-            self._stopTime = di["_stopTime"]
+            self.id = di["_id"]
+            self.speed = di["_speed"]
+            self.minFloor = di["_minFloor"]
+            self.maxFloor = di["_maxFloor"]
+            self.closeTime = di["_closeTime"]
+            self.openTime = di["_openTime"]
+            self.startTime = di["_startTime"]
+            self.stopTime = di["_stopTime"]
 
 
 
