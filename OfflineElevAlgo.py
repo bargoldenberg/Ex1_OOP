78532e8ba548c874fbed1c0c5957ac4692317037
barg991 <barg991@gmail.com>
2021-11-12 23:23:57 +0200

    loadCalls function

--- OfflineElevAlgo.py
+++ OfflineElevAlgo.py
@@ -1,17 +1,14 @@
 import csv
-import Call
+from Call import *
 
-
-
-def callRead( file_name):
+def loadCalls(file_name):
     if __name__ == '__main__':
         rows = []
         calls = []
         with open(file_name) as e_calls:
             csvreader = csv.reader(e_calls)
             for row in csvreader:
-                c = Call.Call(row[0], row[1], row[2], row[3], row[4],row[5])
-                #c = Call(call = rows[0],time=rows[1] , src=rows[2], dest=rows[3] ,elevator=rows[4] ,F=rows[5])
+                c = Call(row[0], row[1], row[2], row[3], row[4],row[5])
                 calls.append(c)
                 rows.append(row)
     return calls
@@ -20,5 +17,6 @@ def callRead( file_name):
 this is a test for the csv read
 """
 c = []
-c = callRead("Calls_a.csv")
-print(c.pop(0))
+c = loadCalls("Calls_a.csv")
+c[1].elevator=1
+print(c)
