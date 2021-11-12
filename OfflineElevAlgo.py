34b405ad88bf3d2ee94a1980e0bdfbbb7c3a845e
sappir <sappirbohbot@gmail.com>
2021-11-12 23:15:16 +0200

    added repr2

--- OfflineElevAlgo.py
+++ OfflineElevAlgo.py
@@ -8,17 +8,17 @@ def callRead( file_name):
         rows = []
         calls = []
         with open(file_name) as e_calls:
-            cvsreader = csv.reader(e_calls)
-            for row in cvsreader:
+            csvreader = csv.reader(e_calls)
+            for row in csvreader:
                 c = Call.Call(row[0], row[1], row[2], row[3], row[4],row[5])
                 #c = Call(call = rows[0],time=rows[1] , src=rows[2], dest=rows[3] ,elevator=rows[4] ,F=rows[5])
                 calls.append(c)
                 rows.append(row)
-    return rows
+    return calls
 
 """
 this is a test for the csv read
 """
 c = []
 c = callRead("Calls_a.csv")
-print(c.__repr__())
+print(c.)
