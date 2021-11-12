f3fc8cff866b35b9c8e1e55c0b8d05571515c31e
sappir <sappirbohbot@gmail.com>
2021-11-12 22:30:03 +0200

    added repr2

--- Call.py
+++ Call.py
@@ -1,4 +1,4 @@
-
+import csv
 
 class Call:
     def __init__(self, call = "", time= 0.0 , src = 0, dest = 0, elevator = 0,F=-1 ):
@@ -8,4 +8,15 @@ class Call:
         self.dest = dest
         self.elevator = elevator
         self.F = F
+"""""""""
+    def from_Csv(self, file_name):
+        with open(file_name) as fp:
+            di = csv.load(fp)
+            self.call = di["_id"]
+            self.time = di["_speed"]
+            self.src = di["_minFloor"]
+            self.dest = di["_maxFloor"]
+            self.elevator = di["_closeTime"]
+            self.F = di["_openTime"]
+"""
 
--- OfflineElevAlgo.py
+++ OfflineElevAlgo.py
@@ -1,2 +1,17 @@
 import csv
+import Call
+
+
+
+def callRead( file_name):
+    if __name__ == '__main__':
+        rows = []
+        calls = []
+        with open(file_name) as e_calls:
+            cvsreader = csv.reader(e_calls)
+            for rows in cvsreader:
+                c = Call(call = rows[0],time=rows[1] , src=rows[2], dest=rows[3] ,elevator=rows[4] ,F=rows[5])
+                calls.append(c)
+                rows.append(rows)
+    return calls
 
