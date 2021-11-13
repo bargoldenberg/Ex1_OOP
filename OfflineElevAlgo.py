ef581fff977b4f4bc4031639c940665a1126823e
sappir <sappirbohbot@gmail.com>
2021-11-13 19:28:36 +0200

    added csv

--- OfflineElevAlgo.py
+++ OfflineElevAlgo.py
@@ -13,10 +13,34 @@ def loadCalls(file_name):
                 rows.append(row)
     return calls
 
+def exportCalls(e_call_assigned):
+    new_e_call = []
+    for call in e_call_assigned:
+        new_e_call.append(call.__dict__.values())
+    print(new_e_call) # ONLY FOR Debug.
+    fileName = 'Assigned_calls.csv'
+
+    with open(fileName , 'w',newline="") as file:
+        csvwriter = csv.writer(file)
+        for element in new_e_call:
+            csvwriter.writerow(element)
+
+
+
+
+
+
+
+
+
 """
 this is a test for the csv read
 """
+
+
 c = []
 c = loadCalls("Calls_a.csv")
 c[1].elevator=1
 print(c)
+print("Check for exporting:")
+exportCalls(c)
